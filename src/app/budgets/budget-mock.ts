368d7d20fc21ea428fe743de07c24cf11d8208bc
Franziska-Voegele <FranziVoegele@web.de>
2021-05-25 20:47:33 +0200

    UX in der Budget-View

---
+++ src/app/budgets/budget-mock.ts
@@ -0,0 +1,62 @@
+import { Budget } from '../budget.model';
+
+export const BUDGETS: Budget[] = [
+    {
+      "id": 1,
+      "kategorie": "Freizeit",
+      "betrag": 10,
+    
+    },
+    {
+      "id": 2,
+      "kategorie": "Essen und Trinken",
+      "betrag": 1.5,
+      
+    },
+    {
+      "id": 3,
+      "kategorie": "Reisen",
+      "betrag": 3.5,
+      
+    },
+
+    {
+      "id": 4,
+      "kategorie": "Infrastruktur",
+      "betrag": 4,
+      
+    },
+    {
+      "id": 5,
+      "kategorie": "Mobilität",
+      "betrag": 5,
+      
+    },
+    {
+      "id": 6,
+      "kategorie": "Bekleidung",
+      "betrag": 5,
+      
+    },
+    {
+      "id": 5,
+      "kategorie": "Ausbildung und Studium",
+      "betrag": 5,
+      
+    },
+    {
+      "id": 5,
+      "kategorie": "Versicherung",
+      "betrag": 5,
+      
+    },
+    {
+      "id": 5,
+      "kategorie": "Sonstige Ausgaben",
+      "betrag": 5,
+      
+    },
+
+    
+
+]
--- src/app/budgets/budgets.component.ts
+++ src/app/budgets/budgets.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Budget } from '../budget.model';
+import { BUDGETS } from './budget-mock';
+
 
 @Component({
   selector: 'budgets',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budgets.component.scss']
 })
 export class BudgetsComponent implements OnInit {
+  budgets: Budget[] = BUDGETS;
+  neueAusgabe: Budget = new Budget();
+  kategorien: String[] = ["Essen und Trinken", "Reisen", "Infrastruktur", "Mobilität", "Bekleidung", "Freizeit", "Ausbildung und Studium", "Versicherungen", "Sonstige Ausgaben"];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // nach Datum sortieren
+    this.budgets.sort();
+  }
+
+  hinzufuegen(ausgabe: Budget) {
+    console.log('Hinzufügen:')
+    console.log(ausgabe);
+    // Hinzufügen und nach Datum sortieren
+    this.budgets.push(this.neueAusgabe);
+    
+    // Hinzufügen-Form resetten
+    this.neueAusgabe = new Budget();
+  }
+
+  aktualisieren(ausgabe: Budget) {
+    console.warn('Aktualisiere:')
+    console.log(ausgabe);
+    // backend-Aufruf zum Aktualisieren
+
+  }
+
+  loeschen(ausgabe: Budget) {
+    console.warn('Lösche:')
+    console.log(ausgabe);
+    // backend-Aufruf zum Löschen
+
+
+    // frontend-seitiges löschen
+    this.budgets = this.budgets.filter( einzelneAusgabe => einzelneAusgabe.id !== ausgabe.id );
+  }
+
+  getErrorMessage(formField: any){
+    if(formField.hasError("required")) return "Pflichtfeld";
+
+    if(formField.hasError("min") && formField.control.errors.min.actual < formField.control.errors.min.min) {
+      return "Der Wert darf nicht negativ sein";
+    }
+
+    else return "";
   }
 
 }
