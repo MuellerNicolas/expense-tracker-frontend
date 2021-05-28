90e595284e7d28c6983e1974a13fc15e112510fa
Nicolas Mueller <nicolas.moritz.mueller@web.de>
2021-05-28 10:40:40 +0200

    added budget service and tests

--- src/app/budgets/budgets.component.ts
+++ src/app/budgets/budgets.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Budget } from '../budget.model';
-import { BUDGETS } from './budget-mock';
-
+import { Budget } from './budget.model';
+import { BudgetsService } from './budgets.service';
 
 @Component({
   selector: 'budgets',
@@ -9,20 +8,25 @@ import { BUDGETS } from './budget-mock';
   styleUrls: ['./budgets.component.scss']
 })
 export class BudgetsComponent implements OnInit {
-  budgets: Budget[] = BUDGETS;
+  budgets: Budget[] = [];
 
-  constructor() {}
+  constructor(private budgetsService: BudgetsService) {}
 
   ngOnInit(): void {
-    // alphabetisch sortieren
-    this.budgets.sort((a: Budget, b: Budget) => a.kategorie!.localeCompare(b.kategorie!));
+    this.getBudgets();
   }
 
-  aktualisieren(budget: Budget) {
-    console.warn('Aktualisiere:')
-    console.log(budget);
-    // backend-Aufruf zum Aktualisieren
+  getBudgets(): void {
+    this.budgetsService.getBudgets().subscribe(budgets => {
+      this.budgets = budgets;
+      // Nach Kategorie alphabetisch ordnen
+      this.budgets.sort((a: Budget, b: Budget) => a.kategorie!.localeCompare(b.kategorie!))
+    });
+  }
 
+  update(budget: Budget): void {
+    if(!budget) return;
+    this.budgetsService.updateBudget(budget).subscribe();
   }
 
   getErrorMessage(formField: any){
@@ -30,9 +34,7 @@ export class BudgetsComponent implements OnInit {
 
     if(formField.hasError("min") && formField.control.errors.min.actual < formField.control.errors.min.min) {
       return "Der Wert darf nicht negativ sein";
-    }
-
-    else return "";
+    } else return "";
   }
 
 }
---
+++ src/app/budgets/budgets.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Budget } from './budget.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BudgetsService {
+
+  backendAPI: string = environment.backendAPI;
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.error(`${operation} failed: ${error.message}`);
+      // Default-Wert zurückgeben
+      return of(result as T);
+    };
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  getBudgets(): Observable<Budget[]> {
+    return this.httpClient.get<Budget[]>(`${this.backendAPI}/budgets`)
+    .pipe(
+      catchError(this.handleError<Budget[]>('getBudget', []))
+    );
+  }
+
+  updateBudget(budget: Budget): Observable<Budget> {
+    return this.httpClient.put<Budget>(`${this.backendAPI}/budgets/${budget.id}`, budget, this.httpOptions)
+    .pipe(
+      catchError(this.handleError<any>('updateBudget'))
+    );
+  }
+}
