407caddc0c2dcfaad6c02772b36e4936db136c55
Nicolas Mueller <nicolas.moritz.mueller@web.de>
2021-06-04 12:24:15 +0200

    test intially working

--- cypress/integration/budget-e2e-test.spec.ts
+++ cypress/integration/budget-e2e-test.spec.ts
@@ -1,20 +1,40 @@
-it('eingabe test', () => {
-  //Visit the view Budget on the website
-  cy.visit('/budgets');
-  cy.get('#mat-expansion-panel-header-0').click();
+describe('Budgets E2E Test', () => {
+  beforeEach(() => {
+    // Define Intercept for the Budget Data
+    cy.intercept('api/budgets/', { fixture: 'budgets' }).as('getBudgets');
+    // Visit the view Budget on the website
+    cy.visit('/budgets');
+    // wait for the intercepted data anwser
+    cy.wait(['@getBudgets']);
+  });
 
-  //find the field and fill out
-  cy.get('#mat-input-0').clear().type('2000');
+  it('eingabe test', () => {
+    // commands below are waiting till the call is ready / data is present
+    cy.get('#mat-expansion-panel-header-0').click();
 
-  // clicking button for updateing the value
-  cy.get('[type=submit]').first().click();
-  cy.wait(1000);
+    // find the field and fill out
+    cy.get('#mat-input-0').clear().type('2000');
 
-  //Reload the homepage to check whether the new value has been saved.
-  cy.reload();
-  //Open the Category again
-  cy.get('#mat-expansion-panel-header-0').click();
+    // check if Update PUT HTTP gets called
+    // cy.intercept('PUT', 'api/budgets/1', (req) => {
+    //   expect(req.body).to.include('2000');
+    // });
 
-  //validate the input field
-  cy.get('#mat-input-0').should('have.value', '2000');
+    cy.intercept('api/budgets/1', 'success').as('update');
+
+    // clicking button for updating the value
+    cy.get('[type=submit]').first().click();
+
+    // cy.wait(['@update']).then((interception) => {
+    //   console.log(interception);
+    // });
+    cy.wait('@update')
+      .its('response.statusCode')
+      .should('eq', 200) // 200 OK;
+      .its('request.body')
+      .should('include', 'success');
+
+    // validate the input field
+    cy.get('#mat-input-0').should('have.value', '2000');
+  });
 });
