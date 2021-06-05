31c1187dbb39364b78e3323129d55f07461aa6cb
Nicolas Mueller <nicolas.moritz.mueller@web.de>
2021-06-05 10:09:02 +0200

    fixed e2e test

--- cypress/integration/budget-e2e-test.spec.ts
+++ cypress/integration/budget-e2e-test.spec.ts
@@ -7,7 +7,6 @@ describe('Budgets E2E Test', () => {
     // wait for the intercepted data anwser
     cy.wait(['@getBudgets']);
   });
-
   it('eingabe test', () => {
     // commands below are waiting till the call is ready / data is present
     cy.get('#mat-expansion-panel-header-0').click();
@@ -15,16 +14,17 @@ describe('Budgets E2E Test', () => {
     // find the field and fill out
     cy.get('#mat-input-0').clear().type('2000');
 
-    cy.intercept('api/budgets/1', 'success').as('update');
+    cy.intercept('api/budgets/1').as('update');
 
     // clicking button for updating the value
     cy.get('[type=submit]').first().click();
 
-    cy.wait('@update')
-      .its('response.statusCode')
-      .should('eq', 200) // 200 OK;
-      .its('request.body')
-      .should('include', 'success');
+    // confirm outgoing request
+    cy.get('@update').its('request.body').should('deep.equal', {
+      budget: 2000,
+      budgetId: '1',
+      kategorie: 'Ausbildung und Studium',
+    });
 
     // validate the input field
     cy.get('#mat-input-0').should('have.value', '2000');
