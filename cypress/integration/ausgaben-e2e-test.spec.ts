019ce5c9e83988ab82bbaf6fcd88d6a70303387a
Nicolas Mueller <nicolas.moritz.mueller@web.de>
2021-06-06 12:35:58 +0200

    added new tests for new expense

--- cypress/integration/ausgaben-e2e-test.spec.ts
+++ cypress/integration/ausgaben-e2e-test.spec.ts
@@ -12,7 +12,8 @@ describe('Ausgaben E2E Test', () => {
     // wait for the intercepted data anwser
     cy.wait(['@getAusgaben', '@getBudgets']);
   });
-  it('should add new Expense', () => {
+
+  it('should add new expense', () => {
     //click the date picker
     cy.get('[type=button]').first().click();
     //choose date 15
@@ -37,4 +38,37 @@ describe('Ausgaben E2E Test', () => {
       datum: '2021-06-14T22:00:00.000Z',
     });
   });
+
+  it('should not add new expense with missing field', () => {
+    //click the date picker
+    cy.get('[type=button]').first().click();
+    //choose date 15
+    cy.contains('15').click();
+    // don't input all fields
+    cy.get('#mat-input-2').type('40');
+    cy.get('#mat-select-0').click();
+    cy.get('#mat-option-2').click();
+    cy.get('[type=submit]').first().should('be.disabled');
+  });
+
+  it('should not add new expense with a negative "betrag" value', () => {
+    //click the date picker
+    cy.get('[type=button]').first().click();
+    //choose date 15
+    cy.contains('15').click();
+    // find the other fields and fill out with negative betrag
+    cy.get('#mat-input-1').type('Pizza essen');
+    cy.get('#mat-input-2').type('-40');
+    cy.get('#mat-select-0').click();
+    cy.get('#mat-option-2').click();
+    cy.get('[type=submit]').first().should('be.disabled');
+  });
+
+  it('should update an existing expense', () => {});
+
+  it('should not update an existing expense with missing fields', () => {});
+
+  it('should not update an existing expense with a negative "betrag" value', () => {});
+
+  it('should update an delete expense', () => {});
 });
