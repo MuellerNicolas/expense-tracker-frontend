4d4141d2185e4aa9394b94a50e026f190c410a50
Nicolas Mueller <nicolas.moritz.mueller@web.de>
2021-06-13 11:54:42 +0200

    added unit test for error msg

--- src/app/budgets/budgets.component.spec.ts
+++ src/app/budgets/budgets.component.spec.ts
@@ -35,4 +35,50 @@ describe('BudgetsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return error message "Pflichtfeld"', () => {
+    // FormField-Mock
+    const formField = {
+      hasError(input: string): boolean {
+        if (input === 'required') return true;
+        if (input === 'min') return false;
+        else return false;
+      },
+    };
+    expect(component.getErrorMessage(formField)).toBe('Pflichtfeld');
+  });
+
+  it('should return error message "Der Wert darf nicht negativ sein"', () => {
+    // FormField-Mock
+    const formField = {
+      control: {
+        errors: {
+          min: {
+            actual: -1,
+            min: 0,
+          },
+        },
+      },
+      hasError(input: string): boolean {
+        if (input === 'required') return false;
+        if (input === 'min') return true;
+        else return false;
+      },
+    };
+    expect(component.getErrorMessage(formField)).toBe(
+      'Der Wert darf nicht negativ sein'
+    );
+  });
+
+  it('should return an empty error message', () => {
+    // FormField-Mock
+    const formField = {
+      hasError(input: string): boolean {
+        if (input === 'required') return false;
+        if (input === 'min') return false;
+        else return false;
+      },
+    };
+    expect(component.getErrorMessage(formField)).toBe('');
+  });
 });
